c2f43bfe93a330071b4702b2e2075c1b0f8ecd1e
Jonny <jonny.jw1@gmail.com>
2021-05-04 23:35:49 -0300

    inital commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# jpedu-autenticacao
---
+++ src/main/java/com/br/pmjp/dtic/jpedu/autenticacao/jpedu/autenticacao/Application.java
@@ -0,0 +1,13 @@
+package com.br.pmjp.dtic.jpedu.autenticacao.jpedu.autenticacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
